80320645b10fa95fa05f157442a43135c14efdc9
CoderBrothers <121336523+CoderBrothers@users.noreply.github.com>
2024-08-03 10:27:07 +0300

    Add project files.

---
+++ PhoneBook/Abonent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    public class Abonent
+    {
+        public string Name {  get; set; }
+        public string Tel { get; set; }
+        public Abonent(string name, string tel) => (Name, Tel) = (name, tel);
+    }
+}
