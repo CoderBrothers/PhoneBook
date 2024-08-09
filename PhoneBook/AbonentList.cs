d84671f5af829f4c8c8f00a1e322d28ddd278d1e
DebugDynasty <zilli7733@gmail.com>
2024-08-09 19:13:58 +0300

    Added method FindAbonentsByName

--- PhoneBook/AbonentList.cs
+++ PhoneBook/AbonentList.cs
@@ -13,15 +13,34 @@ namespace PhoneBook
         {
             _list = new List<Abonent>();
         }
-        
         public void AddAbonent(Abonent ab)
         {
+            if (ab == null || string.IsNullOrWhiteSpace(ab.Name) || string.IsNullOrWhiteSpace(ab.Tel))
+            {
+                Console.WriteLine("You are an idiot: Subscriber cannot be empty, and name and phone number cannot be empty or consist only of spaces.");
+                return;
+            }
             _list.Add(ab);
             Console.WriteLine("Abonent added");
         }
-        public List<Abonent> ShowAllAbonents() => _list;
+
+        public List<Abonent> ShowAllAbonents()
+        {
+            if (_list.Count == 0)
+            {
+                Console.WriteLine("No abonents in the list.");
+            }
+            return _list;
+        }
+
         public void RemoveAbonent(string name, string tel)
         {
+            if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(tel))
+            {
+                Console.WriteLine("You are an idiot: Subscriber cannot be empty, and name and phone number cannot be empty or consist only of spaces.");
+                return;
+            }
+
             Abonent abonentToRemove = _list.Find(ab => ab.Name == name && ab.Tel == tel);
             if (abonentToRemove != null)
             {
@@ -30,7 +49,38 @@ namespace PhoneBook
             }
             else
             {
-                Console.WriteLine("We can not find this abonent");
+                Console.WriteLine("Oops - Abonent not found.");
+            }
+        }
+
+        public void FindAbonentsByName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                Console.WriteLine("Oops - Name cannot be empty or contain only spaces.");
+                return;
+            }
+
+            List<Abonent> foundAbonents = new List<Abonent>();
+            foreach (Abonent ab in _list)
+            {
+                if (ab.Name == name)
+                {
+                    foundAbonents.Add(ab);
+                }
+            }
+
+            if (foundAbonents.Count > 0)
+            {
+                Console.WriteLine("Found Abonents:");
+                foreach (Abonent ab in foundAbonents)
+                {
+                    Console.WriteLine($"{ab.Name} - {ab.Tel}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No abonents found with that name.");
             }
         }
     }
--- PhoneBook/Program.cs
+++ PhoneBook/Program.cs
@@ -6,17 +6,22 @@
         {
             AbonentList abonentList = new AbonentList();
             abonentList.AddAbonent(new Abonent("Bob", "0"));
+            abonentList.AddAbonent(new Abonent("Foo", "1"));
+            abonentList.AddAbonent(new Abonent("Bob", "2"));
+            abonentList.AddAbonent(new Abonent("Bob", "3"));
+            abonentList.AddAbonent(new Abonent("Jack", "2"));
             var test = abonentList.ShowAllAbonents();
             foreach (var abonent in test)
             {
                 Console.WriteLine(abonent.Name + " " + abonent.Tel);
             }
-            abonentList.RemoveAbonent("Bob", "0");
+            abonentList.RemoveAbonent("Bob", "34");
             var test2 = abonentList.ShowAllAbonents();
             foreach (var abonent in test2)
             {
                 Console.WriteLine(abonent.Name + " " + abonent.Tel);
             }
+            abonentList.FindAbonentsByName("  ");
         }
     }
 }
